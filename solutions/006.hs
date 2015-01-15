f85274d0574120e56b18c867b34fe1e68b1446f4
Chad Gilbert <chad.gilbert@cqlcorp.com>
2015-01-14 22:58:26 -0500

    Problems 6 and 7

---
+++ solutions/006.hs
@@ -0,0 +1,8 @@
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = True
+isPalindrome list = left == right
+  where
+    num = div (length list) 2
+    left = take num list
+    right = (take num . reverse) list
+
