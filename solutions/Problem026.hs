5cc6e41454f5a791b79e6878a00dde4c4ac7ab1d
Chad Gilbert <chad.gilbert@cqlcorp.com>
2015-01-20 23:00:03 -0500

    Oops, I was using the wrong combinations function

--- solutions/Problem026.hs
+++ solutions/Problem026.hs
@@ -8,5 +8,6 @@ combinations 1 xs = [ [x] | x <- xs ]
 combinations n (x:xs) = [ x : ys | ys <- combinations (n-1) xs ] ++ combinations n xs
 
 combinations' 0 _ = [[]]
-combinations' n (x:xs) = [ x : ys | ys <- combinations (n-1) xs ] ++ combinations n xs
+combinations' _ [] = []
+combinations' n (x:xs) = [ x : ys | ys <- combinations' (n-1) xs ] ++ combinations' n xs
 
