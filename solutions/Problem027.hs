037229e42f9ddf64d6fb4edb880e216824b2fa75
Chad Gilbert <chad.gilbert@cqlcorp.com>
2015-01-24 22:25:03 -0500

    A more elegant solution to 27

--- solutions/Problem027.hs
+++ solutions/Problem027.hs
@@ -3,7 +3,7 @@ module Problem027 where
 group :: [Int] -> [a] -> [[[a]]]
 group [] _ = [[]]
 group _ [] = []
-group (n:ns) xs = [ comb : rest | (comb,rem) <- combinations'' n xs [],
+group (n:ns) xs = [ comb : rest | (comb,rem) <- combinations''' n xs,
                                   rest <- group ns rem ]
 
 combinations'' :: Int -> [a] -> [a] -> [([a],[a])]
@@ -11,3 +11,13 @@ combinations'' 0 xs unused = [([],unused++xs)]
 combinations'' _ [] _ = []
 combinations'' n (x:xs) unused =
   [ (x : ys,rest) | (ys,rest) <- combinations'' (n-1) xs (unused) ] ++ combinations'' n xs (unused++[x])
+
+combinations''' :: Int -> [a] -> [([a],[a])]
+combinations''' 0 xs = [([],xs)]
+combinations''' _ [] = []
+combinations''' n (x:xs) = this ++ rest
+  where
+    this = [ (x:y,us) | (y,us) <- combinations''' (n - 1) xs ]
+    rest = [ (y,x:us) | (y,us) <- combinations''' n xs ]
+
+
