791d20ffed0e3dad61210b362ca46310ca310a2f
Chad Gilbert <chad.gilbert@cqlcorp.com>
2015-03-01 09:50:51 -0500

    Logic 'n stuff

---
+++ solutions/Problem046.hs
@@ -0,0 +1,37 @@
+module Problem04 where
+
+and', or', nand', nor', xor', impl', eq' :: Bool -> Bool -> Bool
+
+table :: (Bool -> Bool -> Bool) -> IO ()
+
+and' True b = b
+and' False _ = False
+
+or' False b = b
+or' True _ = True
+
+nand' True True = False
+nand' _ _ = True
+
+nor' False False = True
+nor' _ _ = False
+
+xor' a b = not' $ eq' a b
+
+impl' a b = or' (not' a) b
+
+eq' True True = True
+eq' False False = True
+eq' _ _ = False
+
+not' :: Bool -> Bool
+not' True = False
+not' False = True
+
+table f = do
+  let p a b = print (show a ++ " " ++ show b ++ " " ++ show (f a b))
+  p True True
+  p True False
+  p False True
+  p False False
+  return ()
